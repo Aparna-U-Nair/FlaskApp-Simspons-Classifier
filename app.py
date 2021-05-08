68a9de8d2a3826abf1b6bd32450f5b54460bf189
unknown <nairuaparna@gmail.com>
2021-05-08 15:15:11 +0530

    updated the application

--- app.py
+++ app.py
@@ -11,28 +11,29 @@ app = Flask(__name__)
 
 model = load_model("./model_weights/vgg9.h5")
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/predict", methods=["GET","POST"])
-def img_upload():
-    if request.method=="POST":
-        #get the image and preprocess with the above ftn.
-        img_file = request.files["file"] #file keyword given in index.html
 
-        #save the input img to uploads folder
-        upload = os.path.dirname(__file__) #get the root dir of this folder
-        file_path = os.path.join(upload,"uploads",secure_filename=(img_file.filename))
+@app.route("/predict", methods=["GET", "POST"])
+def img_upload():
+    if request.method == "POST":
+        # get the image and preprocess with the above ftn.
+        img_file = request.files["file"]  # file keyword given in index.html
+
+        # save the input img to uploads folder
+        upload = os.path.dirname(__file__)  # get the root dir of this folder
+        file_path = os.path.join(upload, "uploads", secure_filename=(img_file.filename))
         img_file.save(file_path)
 
-        #Call the function to predict
-        true, pred = img_pred(file_path,model)
-        result = "TRUE: "+true+ " and PREDICTION: "+pred
+        # Call the function to predict
+        true, pred = img_pred(file_path, model)
+        result = "TRUE: " + true + " and PREDICTION: " + pred
         return result
     return None
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
-
+    app.run(debug=True)
